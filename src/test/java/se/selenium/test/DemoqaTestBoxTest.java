5a88fe6c13a4abe9843d604107f093fb43eb2427
krishna <krishnapriya.gokulakrishnan@utb.ecutbildning.se>
2026-05-11 14:51:21 +0200

    Add test print messages tested again

--- src/test/java/se/selenium/test/DemoqaTestBoxTest.java
+++ src/test/java/se/selenium/test/DemoqaTestBoxTest.java
@@ -57,6 +57,7 @@ public class DemoqaTestBoxTest {
         System.out.println("TEST PASSED!");
         System.out.println(" Form submitted and verified!");
         System.out.println(" Form submitted and verified and test pass!");
+        System.out.println(" Form submitted and verified and test pass!");
     }
     @AfterEach
     void teardown() {
