daf6271d82eecad929395e65fd49e552f07e6cd7
krishna <krishnapriya.gokulakrishnan@utb.ecutbildning.se>
2026-05-12 09:55:56 +0200

    check again

--- src/test/java/se/selenium/test/DemoqaTestBoxTest.java
+++ src/test/java/se/selenium/test/DemoqaTestBoxTest.java
@@ -58,6 +58,7 @@ public class DemoqaTestBoxTest {
         System.out.println(" Form submitted and verified!");
         System.out.println(" Form submitted and verified and test pass!");
         System.out.println("fix push target again!");
+        System.out.println("check again!");
 
     }
     @AfterEach
